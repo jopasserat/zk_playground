db539b0b2c3c63d662b9dce8d9b95e1379f821c2
Jonathan Passerat-Palmbach <j.passerat-palmbach@imperial.ac.uk>
2019-09-25 12:20:32 +0100

    enh: allow workdir to be changed from CLI for deployment artifacts

--- zokrates/bin/deploy.sh
+++ zokrates/bin/deploy.sh
@@ -6,13 +6,15 @@
 # Then runs migration to deploy (to local ganache chain for the moment)
 #
 # $1: path to generated Verifier contract
+# $2: root path where to copy the web3 files to (contracts, truffle config, ...)
 #
 ###
 
-WORKDIR=zokrates+web3js
 verifier_contract=${1:-output/hello_world/verifier.sol}
+WORKDIR=${2:-zokrates+web3js}
+SOURCE_DIR=$(readlink -f $(dirname $0))
 
-mkdir -p "${WORKDIR}"
+mkdir -p "${WORKDIR}/contracts"
 cp "${verifier_contract}" "${WORKDIR}/contracts/"
 cd "${WORKDIR}" || exit 1
 truffle compile
